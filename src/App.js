b67f41ee8d489556b94da34b06e70cadb3aa94d5
kaban421 <amazing.kay@yandex.ru>
2022-10-14 21:52:02 +0200

    change html tegs

--- src/App.js
+++ src/App.js
@@ -11,46 +11,56 @@ class  App extends React.Component {
       <div className="main_body" style={Style}>
 
         <header className="column_header">
-          <div>
-            <img src={Logotip} className="logotip_img" alt="logotip"  />
+          <div className="header_container">
+            <div>
+              <img src={Logotip} className="logotip_img" alt="logotip"  />
+            </div>
+            <h1 className="logotip_text">USATUKirill96</h1>
           </div>
-          <h1 className="logotip">USATUKirill96</h1>
         </header>
 
-        <div className="container">
-          <div className="column_django column">
-            <div className="django_bodu">
-              <a href="" className="header_link">
-                <h3 className="django_header header">Наследование шаблонов в Django</h3>
-              </a>
-              <div className="django_image image">
-                <img src={Picture01} alt="Наследование шаблонов в Django"  />
-              </div>
-            </div>
-          </div>
+        <main className="container">
 
-          <div className="column_include column">
-            <div className="include_bodu">
-             <a href="" className="header_link">
-                <h3 className="cinclude_header header">Встраиваем шаблоны через Include</h3>
-             </a>
-              <div className="include_image image">
-                <img src={Picture02} alt="Встраиваем шаблоны через Include" />
+          <section className="page_django" >
+            <div className="column_django column">
+              <div className="django_bodu">
+                <a href="" className="header_link">
+                  <h3 className="django_header header">Наследование шаблонов в Django</h3>
+                </a>
+                <div className="django_image image">
+                  <img src={Picture01} alt="Наследование шаблонов в Django"  />
+                </div>
               </div>
             </div>
-          </div>
+          </section>
 
-          <div className="column_pythons_exception column">
-            <div className="pythons_exception_bodu">
-              <a href="" className="header_link">
-                <h3 className="pythons_exception_header header">Работа с исключитениями в Python</h3>
-              </a>
-              <div className="pythons_exception_image image">
-                <img src={Picture03} alt="Работа с исключитениями в Python" />
+          <section className="page_include">
+            <div className="column_include column">
+              <div className="include_bodu">
+                <a href="" className="header_link">
+                    <h3 className="cinclude_header header">Встраиваем шаблоны через Include</h3>
+                </a>
+                <div className="include_image image">
+                  <img src={Picture02} alt="Встраиваем шаблоны через Include" />
+                </div>
+              </div>
+           </div>
+          </section>
+
+          <section className="page_pythons_exception">
+            <div className="column_pythons_exception column">
+              <div className="pythons_exception_bodu">
+                <a href="" className="header_link">
+                  <h3 className="pythons_exception_header header">Работа с исключитениями в Python</h3>
+                </a>
+                <div className="pythons_exception_image image">
+                  <img src={Picture03} alt="Работа с исключитениями в Python" />
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </section>
+
+        </main>
 
         <footer className="footer">
           <div className="column_footer">
